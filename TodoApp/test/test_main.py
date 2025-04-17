4d290a4dbb28a76e13eba5066826eb4ad81b7a58
Nikhil-create <nikhilrawal4775@gmail.com>
2025-04-17 12:09:32 +0530

    Some comments

--- TodoApp/test/test_main.py
+++ TodoApp/test/test_main.py
@@ -4,6 +4,8 @@ from fastapi import status
 
 client = TestClient(a)
 
+#some comment
+
 def test_return_health_check():
     response = client.get('/healthy')
     assert response.status_code == status.HTTP_200_OK
