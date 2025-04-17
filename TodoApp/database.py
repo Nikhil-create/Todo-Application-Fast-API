0e65117424ac3154bf7f9a1ff42445829a0117a5
Nikhil-create <nikhilrawal4775@gmail.com>
2025-04-18 01:14:13 +0530

    dabase file

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://nikhil:nikhil@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://nikhil:UvLTBU1TyloRMMZmxfJp3dwvj6HEbs2a@dpg-d00le18dl3ps73e4j7s0-a.virginia-postgres.render.com/todoapp_xx9g'
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
